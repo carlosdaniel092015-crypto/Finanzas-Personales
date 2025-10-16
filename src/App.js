e19a16c3588fcbb65100a92683b0a394a8d7c2b3
carlosdaniel092015-crypto <carlosdaniel092015@gmail.com>
2025-10-16 09:12:04 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -560,28 +560,3 @@ export default function FinanceTracker() {
     </div>
   );
 }
-```
-
-### 6. **.gitignore**
-```
-# dependencies
-/node_modules
-/.pnp
-.pnp.js
-
-# testing
-/coverage
-
-# production
-/build
-
-# misc
-.DS_Store
-.env.local
-.env.development.local
-.env.test.local
-.env.production.local
-
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
